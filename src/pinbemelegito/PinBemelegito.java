0982bb8131753ade062ea3a8d265de103d9b07d1
Katona Valentin Fábió (SZF_2022_2024) <katona.valentin.fabio@diak.szamalk-szalezi.hu>
2023-09-13 13:03:54 +0200

    pin reg ell.

--- src/pinbemelegito/PinBemelegito.java
+++ src/pinbemelegito/PinBemelegito.java
@@ -4,18 +4,27 @@ import java.util.Scanner;
 
 public class PinBemelegito {
 
-    static int pinkod_reg, pinkod_be, probalkozas, maxProbalkozas;
-
+    static int pinkod_reg, pinkod_be, probalkozas, maxProbalkozas, pinMin, pinMax;
+    static boolean joPin, joHossz;
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
+        
         System.out.printf("Pinkód regisztrálása: ");
         pinkod_reg = scn.nextInt();
-        System.out.println("Pinkód elmentve! ");
+        
+        joHossz = pinkod_reg >= pinMin || pinkod_reg <= pinMax;
+        
+        while(!joHossz){
+            System.out.printf("Pinkód regisztrálása: ");
+            pinkod_reg = scn.nextInt();
+            joHossz = pinkod_reg >= pinMin || pinkod_reg <= pinMax;
+        }
+        
+        System.out.println("Pinkód sikeresen beállítva! ");
 
         probalkozas = 1;
         maxProbalkozas = 3;
-        boolean joPin = pinkod_reg == pinkod_be;
+        joPin = pinkod_reg == pinkod_be;
         
         while ((probalkozas <= 3) && !(joPin)) {
             System.out.printf("Kód megadása a belépéshez (%d/%d): ", probalkozas, maxProbalkozas);
